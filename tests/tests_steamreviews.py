50cef7860e3c17152c03e666c567272debcbe44e
Wok <woctezuma@users.noreply.github.com>
2018-05-20 01:21:26 +0200

    Fix unit tests

--- tests/tests_steamreviews.py
+++ tests/tests_steamreviews.py
@@ -6,15 +6,20 @@ import steamreviews
 class TestSteamReviewsMethods(unittest.TestCase):
 
     def test_load_review_dict(self):
-        review_dict = steamreviews.load_review_dict(329070)
-        self.assertDictEqual(review_dict, {'reviews': {}})
+        app_id = 329070
+        review_dict = steamreviews.load_review_dict(app_id)
+        self.assertTrue('reviews' in review_dict)
 
     def test_download_reviews_for_app_id(self):
-        review_dict, query_count = steamreviews.download_reviews_for_app_id(573170)
+        app_id = 573170
+        review_dict, query_count = steamreviews.download_reviews_for_app_id(app_id)
         self.assertGreater(query_count, 0)
 
     def test_download_reviews(self):
-        self.assertTrue(steamreviews.download_reviews([329070, 573170]))
+        app_ids = [329070, 573170]
+        steamreviews.download_reviews(app_ids)
+        review_dict = steamreviews.load_review_dict(329070)
+        self.assertGreater(len(review_dict['reviews']), 1)
 
 
 if __name__ == '__main__':
