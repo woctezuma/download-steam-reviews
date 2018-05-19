e4fbd5fd4050e0f51eb127e97a05fd7cce6a438b
Wok <woctezuma@users.noreply.github.com>
2018-05-20 01:12:08 +0200

    Fix wrong function call in unit test

--- tests/tests_steamreviews.py
+++ tests/tests_steamreviews.py
@@ -10,7 +10,7 @@ class TestSteamReviewsMethods(unittest.TestCase):
         self.assertDictEqual(review_dict, {'reviews': {}})
 
     def test_download_reviews_for_app_id(self):
-        review_dict, query_count = steamreviews.download_reviews(573170)
+        review_dict, query_count = steamreviews.download_reviews_for_app_id(573170)
         self.assertGreater(query_count, 0)
 
     def test_download_reviews(self):
