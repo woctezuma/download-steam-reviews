1631f18d0748fef15f365c95f50d860718f985be
Wok <woctezuma@users.noreply.github.com>
2018-05-20 00:41:23 +0200

    Refactoring (huge)

--- download_reviews.py
+++ download_reviews.py
@@ -1,10 +1,15 @@
-#!/usr/env/bin python
+# Objective: download every Steam review for the games of your choice.
+#
+# Input:
+#   - idlist.txt
+#
+# Output:
+#   - idprocessed.txt
+#   - data/review_APPID.json for each APPID in idlist.txt
+#
+# Reference:
+#   https://raw.githubusercontent.com/CraigKelly/steam-data/master/data/games.py
 
-"""Create idprocessed.txt, and data/review_APPID.json for each APPID in idlist.txt."""
-
-# Heavily inspired from: https://raw.githubusercontent.com/CraigKelly/steam-data/master/data/games.py
-
-import csv
 import json
 import logging
 import pathlib
@@ -13,191 +18,236 @@ import time
 import requests
 
 
-def parse_id(i):
-    """Since we deal with both strings and ints, force appid to be correct."""
+def parse_app_id(app_id):
+    # Objective: return an app_id as an integer, no matter whether the input app_id is a string or an integer.
     try:
-        return int(str(i).strip())
+        return int(str(app_id).strip())
     except ValueError:
         return None
 
 
-def id_reader():
-    """Read the previous created idlist.txt."""
-    with open("idlist.txt") as basefile:
-        reader = csv.reader(basefile)
-        for row in reader:
-            yield parse_id(row[0])
+def get_input_app_ids_filename():
+    # Objective: return the filename where input app_ids are stored.
+    return 'idlist.txt'
 
 
-def get_id_processed_filename(use_date=True):
-    import time
+def app_id_reader(filename=None):
+    # Objective: return a generator of the app_ids to process.
+
+    if filename is None:
+        filename = get_input_app_ids_filename()
+
+    with open(filename, 'r') as f:
+        for row in f.readlines():
+            yield parse_app_id(row)
+
+
+def get_processed_app_ids_filename(filename_root='idprocessed'):
+    # Objective: return the filename where processed app_ids are saved.
 
     # Get current day as yyyymmdd format
-    date_format = "%Y%m%d"
-    current_date = time.strftime(date_format)
+    current_date = time.strftime('%Y%m%d')
 
-    if use_date:
-        id_processed_filename = "idprocessed_on_" + current_date + ".txt"
-    else:
-        id_processed_filename = "idprocessed.txt"
+    processed_app_ids_filename = filename_root + '_on_' + current_date + '.txt'
 
-    return id_processed_filename
+    return processed_app_ids_filename
 
 
-def previous_results():
-    """Return a set of all previous found ID's."""
-    temp_filename = get_id_processed_filename()
-    all_ids = set()
+def get_processed_app_ids():
+    # Objective: return a set of all previously processed app_ids.
+
+    processed_app_ids_filename = get_processed_app_ids_filename()
+
+    all_app_ids = set()
     try:
-        with open(temp_filename, "r") as f:
-            for line in f:
-                appid = parse_id(line)
-                if appid:
-                    all_ids.add(appid)
+        for app_id in app_id_reader(processed_app_ids_filename):
+            all_app_ids.add(app_id)
     except FileNotFoundError:
-        pathlib.Path(temp_filename).touch()
-        print('Creating ' + temp_filename)
-    return all_ids
+        print('Creating ' + processed_app_ids_filename)
+        pathlib.Path(processed_app_ids_filename).touch()
+    return all_app_ids
 
 
-def main():
-    """Entry point."""
+def get_default_request_parameters():
+    # Objective: return a dict of default paramters for a request to Steam API.
+    #
+    # Reference:
+    #   https://gist.github.com/adambuczek/95906b0c899c5311daeac515f740bf33
 
-    import pathlib
+    default_request_parameters = {
+        'json': '1',
+        'language': 'all',  # e.g. english or schinese
+        'filter': 'recent',  # e.g. funny or helpful
+        'review_type': 'all',
+        'purchase_type': 'all',
+    }
 
-    # Data folder
-    data_path = "data/"
+    return default_request_parameters
+
+
+def get_data_path():
+    # Objective: return the path to the directory where reviews are stored.
+
+    data_path = 'data/'
+
     # Reference of the following line: https://stackoverflow.com/a/14364249
     pathlib.Path(data_path).mkdir(parents=True, exist_ok=True)
 
-    logging.basicConfig(level=logging.DEBUG)
-    logging.getLogger('requests').setLevel(logging.DEBUG)
-    log = logging.getLogger(__name__)
+    return data_path
 
-    defaults = {
-        # Copied from: https://gist.github.com/adambuczek/95906b0c899c5311daeac515f740bf33
-        'json': '1',
-        'language': "all",  # e.g. english or schinese
-        'filter': "recent",  # e.g. funny or helpful
-        'review_type': "all",
-        'purchase_type': "all",
+
+def get_steam_api_url():
+    # Objective: return the url of Steam API for reviews.
+
+    return 'http://store.steampowered.com/appreviews/'
+
+
+def get_steam_api_rate_limits():
+    # Objective: return the rate limits of Steam API for reviews.
+
+    rate_limits = {
+        'max_num_queries': 150,
+        'cooldown': (5 * 60) + 10,  # 5 minutes plus a cushion
     }
-    log.info("Default parameters: %s", repr(defaults))
 
-    api_url = "http://store.steampowered.com/appreviews/"
-    query_limit = 150
-    wait_time = (5 * 60) + 10  # 5 minutes plus a cushion
+    return rate_limits
 
-    # Initialization of maximal number of reviews with a large number
-    # NB: Any number greater than 20 should have been ok, since:
-    #   - this variable will be overrided after the first API request,
-    #   - an API request cannot return more than 20 reviews (or so, I read).
-    max_num_reviews = pow(10, 4)
 
-    log.info("Getting previous results from " + get_id_processed_filename())
-    previos_ids = previous_results()
+def get_output_filename(app_id):
+    return get_data_path() + 'review_' + str(app_id) + '.json'
 
-    log.info("Opening " + get_id_processed_filename())
+
+def load_review_dict(review_data_filename):
+    try:
+        with open(review_data_filename, 'r', encoding='utf8') as in_json_file:
+            review_dict = json.load(in_json_file)
+    except FileNotFoundError:
+        review_dict = dict()
+        review_dict['reviews'] = dict()
+
+    return review_dict
+
+
+def get_request(app_id):
+    request = dict(get_default_request_parameters())
+    request['appids'] = str(app_id)
+
+    return request
+
+
+def download_reviews_for_app_id_with_offset(app_id, offset=0):
+    req_data = get_request(app_id)
+    req_data['start_offset'] = str(offset)
+
+    resp_data = requests.get(get_steam_api_url() + req_data['appids'], params=req_data)
+
+    result = resp_data.json()
+
+    success_flag = result['success']
+
+    try:
+        downloaded_reviews = result['reviews']
+        query_summary = result['query_summary']
+    except KeyError:
+        success_flag = False
+        downloaded_reviews = []
+        query_summary = dict()
+
+    return success_flag, downloaded_reviews, query_summary
+
+
+def download_reviews_for_app_id(app_id, query_count=0):
+    rate_limits = get_steam_api_rate_limits()
+
+    data_filename = get_output_filename(app_id)
+    review_dict = load_review_dict(data_filename)
+
+    previous_review_ids = review_dict['reviews']
+
+    num_reviews = None
+
+    offset = 0
+    new_reviews = []
+
+    while (num_reviews is None) or (offset < num_reviews):
+        success_flag, downloaded_reviews, query_summary = download_reviews_for_app_id_with_offset(app_id, offset)
+
+        if success_flag:
+            new_reviews.extend(downloaded_reviews)
+        else:
+            break
+
+        offset += len(downloaded_reviews)
+        query_count += 1
+
+        if num_reviews is None:
+            review_dict['query_summary'] = query_summary
+            num_reviews = query_summary['total_reviews']
+
+        if query_count >= rate_limits['max_num_queries']:
+            cooldown_duration = rate_limits['cooldown']
+            print('Number of queries {} reached. Cooldown: {} seconds'.format(query_count, cooldown_duration))
+            time.sleep(cooldown_duration)
+            query_count = 0
+
+        if any([review['recommendationid'] in previous_review_ids for review in downloaded_reviews]):
+            break
+
+    for review in new_reviews:
+        review_id = review['recommendationid']
+        if review_id not in previous_review_ids:
+            review_dict['reviews'][review_id] = review
+
+    with open(data_filename, 'w') as f:
+        f.write(json.dumps(review_dict) + '\n')
+
+    return review_dict, query_count
+
+
+def download_reviews(input_app_ids=None, previously_processed_app_ids=None):
+    if input_app_ids is None:
+        print('Loading {}'.format(get_input_app_ids_filename()))
+        input_app_ids = [app_id for app_id in app_id_reader()]
+
+    if previously_processed_app_ids is None:
+        print('Loading {}'.format(get_processed_app_ids_filename()))
+        previously_processed_app_ids = get_processed_app_ids()
+
+    logging.basicConfig(level=logging.DEBUG)
+    logging.getLogger('requests').setLevel(level=logging.DEBUG)
+    log = logging.getLogger(name=__name__)
+
     query_count = 0
     game_count = 0
 
-    temp_filename = get_id_processed_filename()
+    for app_id in input_app_ids:
 
-    log.info("Opening idlist.txt")
-    appid_list = [appid for appid in id_reader()]
+        if app_id in previously_processed_app_ids:
+            log.info('Skipping previously found appID = {}'.format(app_id))
+            continue
+        else:
+            log.info('Downloading reviews for appID = '.format(app_id))
 
-    for appid in appid_list:
-
-        output_file = "review_" + str(appid) + ".json"
-        data_filename = data_path + output_file
-
-        if pathlib.Path(data_filename).is_file():
-            if appid in previos_ids:
-                log.info("Skipping previously found id %d", appid)
-                continue
-            else:
-                log.info("Updating previously found id %d", appid)
-
-        try:
-            with open(data_filename, 'r', encoding="utf8") as in_json_file:
-                review_dict = json.load(in_json_file)
-        except FileNotFoundError:
-            review_dict = dict()
-            review_dict["reviews"] = dict()
-
-        previous_review_ids = set(review_dict["reviews"].keys())
-
-        url = api_url + str(appid)
-
-        req_data = dict(defaults)
-        req_data['appids'] = str(appid)
-
-        new_reviews = []
-
-        # Initialize
-        offset = 0
-        num_reviews = max_num_reviews
-        try_count = 0
-        while (offset < num_reviews) and (try_count < 3):
-            req_data['start_offset'] = str(offset)
-
-            resp_data = requests.get(url, params=req_data)
-
-            result = resp_data.json()
-
-            request_success_flag = result['success']
-
-            try:
-                downloaded_reviews = result["reviews"]
-                new_reviews.extend(downloaded_reviews)
-            except KeyError:
-                print('\nThe request returned an empty response with flag: ', str(request_success_flag) + '\n')
-                break
-
-            num_reviews_with_this_request = result["query_summary"]["num_reviews"]
-            offset += num_reviews_with_this_request
-
-            if num_reviews_with_this_request == 0:
-                try_count += 1
-
-            if num_reviews == max_num_reviews:
-                # Real number of reviews for the given appID
-                num_reviews = result["query_summary"]["total_reviews"]
-
-                print(result["query_summary"])
-
-                # To be saved to JSON
-                review_dict["query_summary"] = result["query_summary"]
-
-            query_count += 1
-
-            if query_count >= query_limit:
-                log.info("query count is %d, waiting for %d secs", query_count, wait_time)
-                time.sleep(wait_time)
-                query_count = 0
-
-            if any([review["recommendationid"] in previous_review_ids for review in downloaded_reviews]):
-                break
-
-        for review in [r for r in new_reviews if r["recommendationid"] not in previous_review_ids]:
-            review_id = review["recommendationid"]
-            review_dict["reviews"][review_id] = review
-
-        with open(data_filename, "w") as g:
-            g.write(json.dumps(review_dict) + '\n')
-
-        log.info("Review records written for %s: %d (expected: %d)",
-                 appid, len(review_dict["reviews"]), num_reviews)
+        review_dict, query_count = download_reviews_for_app_id(app_id, query_count)
 
         game_count += 1
 
-        with open(temp_filename, "a") as f:
-            f.write(str(appid) + '\n')
+        with open(get_processed_app_ids_filename(), 'a') as f:
+            f.write(str(app_id) + '\n')
 
-    log.info("Game records written: %d", game_count)
+        log.info('[appID = {}] num_reviews = {} (expected: {}'.format(app_id, len(review_dict['reviews']),
+                                                                      review_dict['query_summary']['total_reviews']))
 
+    log.info('Game records written: {}'.format(game_count))
+
+    return
+
+
+def main():
+    # noinspection PyTypeChecker
+    download_reviews(input_app_ids=None, previously_processed_app_ids=None)
     return True
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
