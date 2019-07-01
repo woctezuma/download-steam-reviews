4e7125885f74e03ceb7fcbe7dc930393ce7b2d6e
Wok <woctezuma@users.noreply.github.com>
2019-07-01 12:04:16 +0200

    Add unit tests for requests with a timestamp threshold

--- tests/tests_steamreviews.py
+++ tests/tests_steamreviews.py
@@ -21,6 +21,26 @@ class TestSteamReviewsMethods(unittest.TestCase):
         review_dict = steamreviews.load_review_dict(329070)
         self.assertGreater(len(review_dict['reviews']), 1)
 
+    def test_download_reviews_with_recent_filter_and_day_range(self):
+        app_id = 239350
+        request_params = dict()
+        request_params['filter'] = 'recent'
+        request_params['day_range'] = '3'
+        _, query_count = steamreviews.download_reviews_for_app_id(app_id,
+                                                                  chosen_request_params=request_params,
+                                                                  verbose=True)
+        self.assertGreater(query_count, 0)
+
+    def test_download_reviews_with_updated_filter_and_day_range(self):
+        app_id = 239350
+        request_params = dict()
+        request_params['filter'] = 'updated'
+        request_params['day_range'] = '3'
+        _, query_count = steamreviews.download_reviews_for_app_id(app_id,
+                                                                  chosen_request_params=request_params,
+                                                                  verbose=True)
+        self.assertGreater(query_count, 0)
+
 
 if __name__ == '__main__':
     unittest.main()
