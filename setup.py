7ce96befcc6429af698a4c8065fc570118ffa939
Wok <woctezuma@users.noreply.github.com>
2020-10-28 14:41:37 +0100

    Standardize setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,31 @@
-from distutils.core import setup
-
-# noinspection PyUnresolvedReferences
 import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='steamreviews',
-    packages=['steamreviews'],
+    version='0.9.2',
+    author='Wok',
+    author_email='wok@tuta.io',
+    description='An API to download Steam reviews',
+    keywords=['steam', 'review', 'reviews', 'download', 'api'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/woctezuma/download-steam-reviews',
+    download_url='https://github.com/woctezuma/download-steam-reviews/archive/0.9.2.tar.gz',
+    packages=setuptools.find_packages(),
     install_requires=[
         'requests',
     ],
-    version='0.9.2',
-    description='An API to download Steam reviews',
-    long_description='Steam Reviews: an API to download Steam reviews, written in Python 3.',
-    long_description_content_type='text/x-rst',
-    author='Wok',
-    author_email='wok@tuta.io',
-    url='https://github.com/woctezuma/download-steam-reviews',
-    download_url='https://github.com/woctezuma/download-steam-reviews/archive/0.9.2.tar.gz',
-    keywords=['steam', 'review', 'reviews', 'download', 'api'],
+    test_suite='nose.collector',
+    tests_require=['nose'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Topic :: Games/Entertainment',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
+    python_requires='>=3',
 )
