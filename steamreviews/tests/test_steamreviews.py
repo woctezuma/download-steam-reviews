5ea9049e4ffb383e0366e58bc42c3bd9cef8cfb0
Wok <woctezuma@users.noreply.github.com>
2023-11-19 21:01:45 +0100

    Apply ruff

--- steamreviews/tests/test_steamreviews.py
+++ steamreviews/tests/test_steamreviews.py
@@ -7,12 +7,12 @@ class TestSteamReviewsMethods(unittest.TestCase):
     def test_load_review_dict(self):
         app_id = 329070
         review_dict = steamreviews.load_review_dict(app_id)
-        self.assertTrue("reviews" in review_dict)
+        assert "reviews" in review_dict
 
     def test_download_reviews_for_app_id(self):
         app_id = 573170
         _, query_count = steamreviews.download_reviews_for_app_id(app_id, verbose=True)
-        self.assertGreater(query_count, 0)
+        assert query_count > 0
 
     def test_download_reviews_filtered_by_language(self):
         app_id = 573170
@@ -23,7 +23,7 @@ class TestSteamReviewsMethods(unittest.TestCase):
             chosen_request_params=request_params,
             verbose=True,
         )
-        self.assertGreater(query_count, 0)
+        assert query_count > 0
 
     def test_download_reviews_filtered_by_positive_review_type(self):
         app_id = 573170
@@ -34,7 +34,7 @@ class TestSteamReviewsMethods(unittest.TestCase):
             chosen_request_params=request_params,
             verbose=True,
         )
-        self.assertGreater(query_count, 0)
+        assert query_count > 0
 
     def test_download_reviews_filtered_by_negative_review_type(self):
         app_id = 573170
@@ -45,7 +45,7 @@ class TestSteamReviewsMethods(unittest.TestCase):
             chosen_request_params=request_params,
             verbose=True,
         )
-        self.assertGreater(query_count, 0)
+        assert query_count > 0
 
     def test_download_reviews_filtered_by_steam_purchase_type(self):
         app_id = 573170
@@ -56,7 +56,7 @@ class TestSteamReviewsMethods(unittest.TestCase):
             chosen_request_params=request_params,
             verbose=True,
         )
-        self.assertGreater(query_count, 0)
+        assert query_count > 0
 
     def test_download_reviews_filtered_by_non_steam_purchase_type(self):
         app_id = 573170
@@ -67,13 +67,13 @@ class TestSteamReviewsMethods(unittest.TestCase):
             chosen_request_params=request_params,
             verbose=True,
         )
-        self.assertGreater(query_count, 0)
+        assert query_count > 0
 
     def test_download_reviews(self):
         app_ids = [329070, 573170]
         steamreviews.download_reviews_for_app_id_batch(app_ids, verbose=True)
         review_dict = steamreviews.load_review_dict(329070)
-        self.assertGreater(len(review_dict["reviews"]), 1)
+        assert len(review_dict["reviews"]) > 1
 
     def test_download_reviews_with_recent_filter_and_day_range(self):
         app_id = 239350
@@ -85,7 +85,7 @@ class TestSteamReviewsMethods(unittest.TestCase):
             chosen_request_params=request_params,
             verbose=True,
         )
-        self.assertGreater(query_count, 0)
+        assert query_count > 0
 
     def test_download_reviews_with_updated_filter_and_day_range(self):
         app_id = 239350
@@ -97,7 +97,7 @@ class TestSteamReviewsMethods(unittest.TestCase):
             chosen_request_params=request_params,
             verbose=True,
         )
-        self.assertGreater(query_count, 0)
+        assert query_count > 0
 
 
 if __name__ == "__main__":
